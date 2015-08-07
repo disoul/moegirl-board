6874b3fec34378a8bef957b7b1940722238106da
disoul <starky_ds@sina.cn>
2015-08-07 17:09:00 +0800

    json

--- src/js/getimage.js
+++ src/js/getimage.js
@@ -1,11 +1,27 @@
 function sendGet(name){
 	var req = new XMLHttpRequest();
-	req.open("GET","http://127.0.0.1:9000/getimage?name=古河渚",true);
+	req.open("GET","http://127.0.0.1:9000/getimage?name="+name,true);
 	req.setRequestHeader("Content-Type","text/plain");
 	req.send();
 	req.onreadystatechange = function(){
 		if (req.readyState == 4 && req.status == 200){
-			document.getElementById("test").src = req.responseText;
+			$("#"+name+" img").attr("src",req.responseText);
 		}
 	}
+}
+
+function getImage(){
+	$.getJSON('./js/data.json',function(data){
+		for (var i = 0;i < data.amount;i++){
+			(function creatImageBlock(name){
+				var div = $('<div></div>');
+				div.attr("id",name);
+				div.addClass("girl-block");
+				var img = '<img src="../image/loading.gif">';
+				$(".main-content").append(div);
+				$("#"+name).append(img);
+				sendGet(name);
+			})(data.girls[i].name);			
+		}
+	});
 }
